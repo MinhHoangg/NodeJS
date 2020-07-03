0a880e771f2442833febd40ffa5521f218cc8d65
MinhHoangg <49479170+MinhHoangg@users.noreply.github.com>
2020-07-03 10:11:16 +0700

    Second summit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => res.send('HELLO MINH!'))
+app.get('/', (req, res) => res.render('index', {
+    name: 'Minh'
+}))
+
+app.get('/users', (req, res) => res.render('users/index', {
+    users: [
+        {id: 1, name: 'Minh'},
+        {id: 2, name: 'Nghia'}
+    ]
+}))
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 var port = 8080;
 
